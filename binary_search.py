b2bf19ca41d2d23a6c3419c0bab86681c79b1e74
Salvador Telo Alves <therightmandev@gmail.com>
2025-01-25 19:22:14 +0000

    initial binary search

--- binary_search.py
+++ binary_search.py
@@ -4,4 +4,21 @@
 # bsearch(5, [1, 2, 3]) returns -1
 
 
-assert 1 == 19
+def bsearch(value, array):
+    index = len(array) // 2
+    if array[index] == value:
+        return index
+    elif value < array[index]:
+        index = index // 2
+        if array[index] == value:
+            return index
+    else:
+        index += (len(array) - index) // 2
+        return index
+
+    return -1
+
+
+assert bsearch(3, [1, 2, 3, 4]) == 2
+assert bsearch(2, [1, 2, 3, 4]) == 1
+assert bsearch(4, [1, 2, 3, 4]) == 3
